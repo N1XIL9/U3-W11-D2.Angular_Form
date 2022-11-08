36f8f3f37d24c8f2d3d1a8891d9e4c77694c54f9
Nicola Lerra <github.nicolalerra@gmail.com>
2022-11-08 16:22:09 +0100

    comm

--- app/src/app/driven/driven.component.ts
+++ app/src/app/driven/driven.component.ts
@@ -8,15 +8,21 @@ import { Hero } from '../classes/hero';
   styleUrls: ['./driven.component.css'],
 })
 export class DrivenComponent implements OnInit {
-  @ViewChild('f') mioForm!: NgForm;
-
   superHero: Hero = new Hero();
+
+  @ViewChild('f') mioForm!: NgForm;
 
   constructor() {}
 
   ngOnInit() {}
 
   createHero() {
+    this.superHero.name = this.mioForm.value.name;
+    this.superHero.alterEgo = this.mioForm.value.alterEgo;
+    this.superHero.power = this.mioForm.value.power;
+    this.superHero.enemy = this.mioForm.value.enemy;
+    this.superHero.planet = this.mioForm.value.planet;
+    this.superHero.weakness = this.mioForm.value.weakness;
     console.log(this.superHero);
   }
 }
--- app/src/app/reactive/reactive.component.ts
+++ app/src/app/reactive/reactive.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null),
+      alterEgo: new FormControl(null),
+      power: new FormControl(null),
+      enemy: new FormControl(null),
+      planet: new FormControl(null),
+      weakness: new FormControl(null),
+    });
   }
 
+  createHero() {
+    console.log(this.form);
+  }
 }
